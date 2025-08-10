d9a68259c8f40927ec931a9997bf533ceac49514
eliastakala <etakala1998@gmail.com>
2025-08-10 15:42:18 +0300

    3.12 done

---
+++ part3/phonebookbackend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.aofrf6q.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+const name = process.argv[3]
+
+const number = process.argv[4]
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+if (process.argv.length === 3) {
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+        console.log(contact.name, contact.number)
+    })
+    mongoose.connection.close()
+    process.exit(1)
+})
+
+} else {
+    const contact = new Contact({
+        name: name,
+        number: number,
+    })
+    contact.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
