64d999f2a71363594f3bc3bf440f6ec57fb65227
Elias Takala <eliastakala@Eliass-MacBook-Pro.local>
2025-12-29 12:04:31 +0200

    4.8 doneish

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,20 +1,20 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  console.log('blog', blog)
-  blog.save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+//   blog.save()
+//     .then((result) => {
+//       response.status(201).json(result)
+//     })
+//     .catch(error => next(error))
 })
 
 module.exports = blogsRouter
---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,48 @@
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        "title": "The Prince",
+        "author": "Machiavelli",
+        "url": "https://www.goodreads.com/book/show/28862.The_Prince",
+        "likes": "114318"
+    },
+    {
+        "title": "The Alchemist",
+        "author": "Coelho",
+        "url": "https://www.goodreads.com/book/show/18144590-the-alchemist",
+        "likes": "1405461"
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+test.only('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  
+  test.only('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+  
+    assert.strictEqual(response.body.length, 2)
+  })
+
+  after(async () => {
+    await mongoose.connection.close()
+  })
