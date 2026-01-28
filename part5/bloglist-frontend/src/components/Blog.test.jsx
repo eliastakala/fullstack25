7dc317296acaa7d5c036577fecfd515199182eb4
eliastakala <etakala1998@gmail.com>
2026-01-28 21:59:48 +0200

    5.16 done

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import BlogForm from './BlogForm'
 import userEvent from '@testing-library/user-event'
 
 test('renders content correctly', () => {
@@ -42,7 +43,6 @@ test('buttons work', async () => {
     likes: 2
   }
 
-  // const mockHandler = vi.fn()
   render(
     <Blog blog={blog} user={user}
     />
@@ -50,17 +50,62 @@ test('buttons work', async () => {
   const button = screen.getByText('show details')
   const userui = userEvent.setup()
   await userui.click(button)
-  screen.debug()
-  // const container = render(<Blog blog={blog} user={user} />)
-  
-  
-  // console.log('screen', screen)
+  const titleElement = screen.queryByText('testing title Remi')
+  expect(titleElement).toBeDefined()
+  const urlElement = screen.queryByText('test url')
+  expect(urlElement).toBeDefined()
+  const likesElement = screen.queryByText(2)
+  expect(likesElement).toBeDefined()
+})
 
-  // expect(mockHandler.mock.calls).toHaveLength(1)
-  // expect(screen.getByText(/component testing is done with react-testing-library/i)).toBeInTheDocument()
-  // expect(screen.getByText(/remi/i)).toBeInTheDocument()
-  // expect(screen.queryByText('is.fi')).not.toBeInTheDocument()
-  // expect(screen.queryByText('2')).not.toBeInTheDocument()
-  // const input = container.querySelector('#titleAndAuthor')
-  // expect(input).toBeInTheDocument()
+test('liking twice', async () => {
+  const user = {
+    name: 'Remi',
+    username: 'remil',
+    id: 'test user id'
+  }
+  const blog = {
+    id: 'testing id',
+    title: 'testing title',
+    author: 'Remi',
+    user: { id: 'test user id' },
+    url: 'test url',
+    likes: 2
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Blog blog={blog} user={user} like={mockHandler}/>
+  )
+  const button = screen.getByText('show details')
+  const userui = userEvent.setup()
+  await userui.click(button)
+
+  const mockUser = userEvent.setup()
+  const likeButton = screen.getByText('like')
+  await mockUser.click(likeButton)
+  await mockUser.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
+
+test('blog creation', async () => {
+  const createBlog = vi.fn()
+  const user = userEvent.setup()
+
+  render(<BlogForm createBlog={createBlog} />)
+
+  const inputTitle = screen.getByRole('textbox', { name: 'title:' })
+  const inputAuthor = screen.getByRole('textbox', { name: 'author:' })
+  const inputUrl = screen.getByRole('textbox', { name: 'url:' })
+  const sendButton = screen.getByText('save')
+  await user.type(inputTitle, 'testing a form...')
+  await user.type(inputAuthor, 'test user...')
+  await user.type(inputUrl, 'test url...')
+  await user.click(sendButton)
+  expect(createBlog.mock.calls).toHaveLength(1)
+  expect(createBlog.mock.calls[0][0].title).toBe('testing a form...')
+  expect(createBlog.mock.calls[0][0].author).toBe('test user...')
+  expect(createBlog.mock.calls[0][0].url).toBe('test url...')
 })
