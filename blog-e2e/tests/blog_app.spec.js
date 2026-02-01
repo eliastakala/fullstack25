421987c15b99bff3e3b8e4ff0f2ef4e06b8f53bf
eliastakala <etakala1998@gmail.com>
2026-02-01 22:30:25 +0200

    part 5 done

--- blog-e2e/tests/blog_app.spec.js
+++ blog-e2e/tests/blog_app.spec.js
@@ -42,6 +42,7 @@ describe('Blog app', () => {
       await expect(page.getByText('wrong credentials')).toBeVisible()
     })
   })
+
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
       await page.getByRole('button', { name: 'login' }).click()
@@ -58,6 +59,75 @@ describe('Blog app', () => {
         await page.getByRole('button', { name: 'save' }).click()
 
         await expect(page.getByText('Blog entry created')).toBeVisible()
+    })
+  })
+
+  describe('When a blog exists', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByRole('button', { name: 'new blog' }).click()
+      await page.getByLabel('title').fill('test title')
+      await page.getByLabel('author').fill('test author')
+      await page.getByLabel('url').fill('test url')
+      await page.getByRole('button', { name: 'save' }).click()
+    })
+
+    test('a blog can be liked', async ({ page }) => {
+        
+        await page.getByRole('button', { name: 'show details' }).click()
+        const likesDiv = page.locator('.likes')
+        await expect(likesDiv).toContainText('0')
+        await page.getByRole('button', { name: 'like' }).click()
+        await expect(likesDiv).toContainText('1')
+    })
+
+    test('blog creator can delete it', async ({ page }) => {
+        
+        await page.getByRole('button', { name: 'show details' }).click()
+        page.on('dialog', dialog => dialog.accept());
+        await page.getByRole('button', { name: 'delete' }).click()
+    })
+
+    test('delete not visible if not own blog', async ({ page, request }) => {
+        await request.post('http://localhost:3003/api/users', {
+          data: {
+            name: 'Remi Lindholm',
+            username: 'remi',
+            password: 'karsee'
+          }
+        })
+        await page.getByRole('button', { name: 'Logout' }).click()
+        await page.getByRole('button', { name: 'login' }).click()
+        await page.getByLabel('username').fill('remi')
+        await page.getByLabel('password').fill('karsee')
+        await page.getByRole('button', { name: 'login' }).click()
+        await page.getByRole('button', { name: 'show details' }).click()
+
+        await expect(page.getByRole('button', { name: 'delete' })).not.toBeVisible()
+    })
+
+    test('blogs sorted in correct order based on likes', async({ page, request }) => {
+        await page.getByRole('button', { name: 'show details' }).click()
+        await page.getByRole('button', { name: 'like' }).click()
+
+        await page.getByLabel('title').fill('another test title')
+        await page.getByLabel('author').fill('another test author')
+        await page.getByLabel('url').fill('another test url')
+        await page.getByRole('button', { name: 'save' }).click()
+
+        const secondBlog = page.locator('.blog').filter({ hasText: 'another test title' });
+        await secondBlog.getByRole('button', { name: 'show details' }).click()
+        const blogs = await page.locator(".title-author").allTextContents()
+        const secondBlogInfo = blogs[1]
+        await secondBlog.getByRole('button', { name: 'like' }).click()
+        await secondBlog.getByRole('button', { name: 'like' }).click()
+        await secondBlog.getByRole('button', { name: 'like' }).click()
+        const updatedBlogs = await page.locator(".title-author").allTextContents()
+        const firstBlogInfo = updatedBlogs[0]
+        expect(secondBlogInfo).toEqual(firstBlogInfo)
     })
   })
 })
