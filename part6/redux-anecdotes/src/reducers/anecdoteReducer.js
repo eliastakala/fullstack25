3d40c4fa2944459ecb3bb6845c6799448488b52d
eliastakala <etakala1998@gmail.com>
2026-02-13 14:40:52 +0200

    6.15 done

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,25 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-// const anecdotesAtStart = [
-//   'If it hurts, do it more often',
-//   'Adding manpower to a late software project makes it later!',
-//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-//   'Premature optimization is the root of all evil.',
-//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-// ]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = anecdote => {
+const asObject = ({ content, id }) => {
   return {
-    content: anecdote,
-    id: getId(),
+    content: content,
+    id: id,
     votes: 0
   }
 }
-
-// const initialState = anecdotesAtStart.map(asObject)
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -27,7 +14,8 @@ const anecdoteSlice = createSlice({
   reducers: {
     createAnecdote(state, action) {
       const content = action.payload
-      state.push(asObject(content))
+      console.log('content', content)
+      state.push(asObject({ content: content.content, id: content.id }))
     },
     voteAnecdote(state, action) {
       const id = action.payload
--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -7,8 +7,24 @@ const getAll = async () => {
     throw new Error('Failed to fetch anecdotes')
   }
 
-  const data = await response.json()
-  return data
+  return await response.json()
 }
 
-export default { getAll }
+const createNew = async (content) => {
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      content: content,
+      votes: 0
+    }),
+  })
+  
+  if (!response.ok) {
+    throw new Error('Failed to create anecdote')
+  }
+  
+  return await response.json()
+}
+
+export default { getAll, createNew }
